beb4bdfb8883fa32ebf74057151ae6c1c79bee80
viethaihust <vuviethaihust@gmail.com>
2024-12-22 19:02:14 +0700

    reset password

--- app/(main)/(home)/thong-tin-ca-nhan/page.tsx
+++ app/(main)/(home)/thong-tin-ca-nhan/page.tsx
@@ -1,19 +1,31 @@
 "use client";
 import { CameraOutlined } from "@ant-design/icons";
-import { Tabs, TabsProps, Upload, UploadProps } from "antd";
-import { signIn, useSession } from "next-auth/react";
+import {
+  Tabs,
+  TabsProps,
+  Upload,
+  UploadProps,
+  Button,
+  Modal,
+  Form,
+  Input,
+} from "antd";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import BookmarkList from "../components/BookmarkList";
 import UserPostList from "../components/UserPostList";
 import { useState } from "react";
 import { fetchWithAuth } from "@/utils/fetchWithAuth";
 import { toast } from "react-toastify";
+import { BACKEND_URL } from "@/lib/constants";
 
 const ProfilePage = () => {
   const { data: session, update } = useSession();
 
   const [userPosts, setUserPosts] = useState<IReviewPost[]>([]);
   const [bookmarks, setBookmarks] = useState<IBookmark[]>([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handlePostDelete = (postId: string) => {
     setUserPosts((prev) => prev.filter((post) => post._id !== postId));
@@ -25,7 +37,7 @@ const ProfilePage = () => {
   const handleImageUpdate = async (secureUrl: string) => {
     try {
       const response = await fetchWithAuth(
-        "http://localhost:8000/api/users/profile/image",
+        `${BACKEND_URL}/api/users/profile/image`,
         {
           method: "PATCH",
           body: JSON.stringify({ image: secureUrl }),
@@ -46,7 +58,7 @@ const ProfilePage = () => {
 
   const uploadProps: UploadProps = {
     name: "image",
-    action: "http://localhost:8000/api/upload/one-image",
+    action: `${BACKEND_URL}/api/upload/one-image`,
     showUploadList: false,
     onChange(info) {
       if (info.file.status === "done") {
@@ -92,6 +104,43 @@ const ProfilePage = () => {
     },
   ];
 
+  const handleResetPassword = async (values: {
+    oldPassword: string;
+    password: string;
+    confirmPassword: string;
+  }) => {
+    const { oldPassword, password, confirmPassword } = values;
+
+    if (password !== confirmPassword) {
+      toast.error("Mật khẩu và xác nhận mật khẩu không khớp");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const response = await fetchWithAuth(
+        `${BACKEND_URL}/api/auth/reset-password`,
+        {
+          method: "POST",
+          body: JSON.stringify({ oldPassword, password }),
+        },
+        session
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to reset password");
+      }
+
+      toast.success("Mật khẩu đã được thay đổi thành công");
+      setIsModalVisible(false);
+    } catch (error) {
+      console.error("Error resetting password:", error);
+      toast.error("Đổi mật khẩu thất bại");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
       <div className="flex items-center gap-5">
@@ -113,10 +162,73 @@ const ProfilePage = () => {
           <div className="text-xl font-semibold">{session?.user?.name}</div>
           <div className="text-gray-500">{session?.user?.email}</div>
         </div>
+        <button
+          type="submit"
+          onClick={() => setIsModalVisible(true)}
+          className="-mt-4 bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded"
+        >
+          Đổi mật khẩu
+        </button>
       </div>
+
       <div className="mt-5">
         <Tabs defaultActiveKey="1" items={items} />
       </div>
+      <Modal
+        title="Đổi mật khẩu"
+        open={isModalVisible}
+        onCancel={() => setIsModalVisible(false)}
+        footer={null}
+        destroyOnClose
+      >
+        <Form onFinish={handleResetPassword} layout="vertical">
+          <Form.Item
+            label="Mật khẩu cũ"
+            name="oldPassword"
+            rules={[
+              { required: true, message: "Vui lòng nhập mật khẩu cũ" },
+              { min: 6, message: "Mật khẩu phải chứa ít nhất 6 ký tự" },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            label="Mật khẩu mới"
+            name="password"
+            rules={[
+              { required: true, message: "Vui lòng nhập mật khẩu mới" },
+              { min: 6, message: "Mật khẩu phải chứa ít nhất 6 ký tự" },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            label="Xác nhận mật khẩu"
+            name="confirmPassword"
+            rules={[
+              { required: true, message: "Vui lòng xác nhận mật khẩu" },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error("Mật khẩu không khớp!"));
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block loading={loading}>
+              Đổi mật khẩu
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 };
